14a712a8c2ca6238c65fb36f29103c4a5bf04b60
Jack Yeung <jyeung.se@gmail.com>
2018-09-06 21:42:03 -0400

    Created intial back-end repository and skeleton models, views, controllers

---
+++ test/system/gifs_test.rb
@@ -0,0 +1,41 @@
+require "application_system_test_case"
+
+class GifsTest < ApplicationSystemTestCase
+  setup do
+    @gif = gifs(:one)
+  end
+
+  test "visiting the index" do
+    visit gifs_url
+    assert_selector "h1", text: "Gifs"
+  end
+
+  test "creating a Gif" do
+    visit gifs_url
+    click_on "New Gif"
+
+    click_on "Create Gif"
+
+    assert_text "Gif was successfully created"
+    click_on "Back"
+  end
+
+  test "updating a Gif" do
+    visit gifs_url
+    click_on "Edit", match: :first
+
+    click_on "Update Gif"
+
+    assert_text "Gif was successfully updated"
+    click_on "Back"
+  end
+
+  test "destroying a Gif" do
+    visit gifs_url
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+
+    assert_text "Gif was successfully destroyed"
+  end
+end
